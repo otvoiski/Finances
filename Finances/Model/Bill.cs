1c17ff62c355ecd48b2fc83881341260aebabf74
Otavio Voiski <otvoiski@live.com>
2020-07-31 16:10:30 -0300

    Added data models in models

--- Finances/Model/Bill.cs
+++ Finances/Model/Bill.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
+using System;
 
 namespace Finances.Model
 {
-    public class BillInterface
+    public class Bill
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        [NotNull]
         public DateTime Date { get; set; }
 
+        [NotNull]
         public double Price { get; set; }
 
+        [NotNull]
         public bool IsPaid { get; set; }
 
         public DateTime? Payment { get; set; }
 
         public string Installment { get; set; }
 
+        [NotNull]
         public string Description { get; set; }
 
+        [NotNull]
         public string Type { get; set; }
     }
 }
---
+++ Finances/Model/Installment.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace Finances.Model
+{
+    public class Installment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public int ScheduleId { get; set; }
+
+        [Indexed]
+        public int BillId { get; set; }
+    }
+}
